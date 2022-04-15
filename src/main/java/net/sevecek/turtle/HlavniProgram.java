0200d39d971f80960f0babb34c652f6b85373354
Szewieczek <helena.szewieczek@gmail.com>
2022-04-15 21:37:50 +0200

    Želva Žofka nakreslí prasátko.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,48 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        zofka.penDown();
+        nakresliPrasatko(zofka);
+    }
+
+    private void nakresliPrasatko(Turtle zofka) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(150);
+            zofka.turnRight(90);
+            zofka.move(200);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(60);
+        zofka.move(150);
+        zofka.turnRight(120);
+        zofka.move(150);
+        zofka.penUp();
+        zofka.turnRight(120);
+        zofka.move(150);
+
+        zofka.turnLeft(30);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnRight(60);
+
+        zofka.move(200);
+        zofka.turnRight(60);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.penUp();
     }
 
 }
